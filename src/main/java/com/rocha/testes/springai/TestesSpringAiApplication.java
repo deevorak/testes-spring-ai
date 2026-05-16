07a3f27521a80c6eac23a6e6db7d52274286c247
Carlos Rocha <carlosesr.inf@gmail.com>
2026-05-16 20:40:16 -0300

    init

---
+++ src/main/java/com/rocha/testes/springai/TestesSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.rocha.testes.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesSpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocha/testes/springai/TestesSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocha.testes.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
