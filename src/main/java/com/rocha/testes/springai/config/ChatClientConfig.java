74d40ba24459ecb16362a74932bb0e7057db71b3
Carlos Rocha <carlosesr.inf@gmail.com>
2026-05-16 21:01:25 -0300

    prompts

---
+++ src/main/java/com/rocha/testes/springai/config/ChatClientConfig.java
@@ -0,0 +1,31 @@
+package com.rocha.testes.springai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+   // @Value("${spring.ai.google.genai.chat.model}")
+   // private String model;
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+
+        return chatClientBuilder
+                //.defaultOptions(ChatOptions.builder().model(model).temperature(0.8))
+                .defaultSystem("""
+                        You are an internal HR assistant. Your role is to help\s
+                        employees with questions related to HR policies, such as\s
+                        leave policies, working hours, benefits, and code of conduct.
+                        If a user asks for help with anything outside of these topics,\s
+                        kindly inform them that you can only assist with queries related to\s
+                        HR policies.
+                        """)
+                .defaultUser("How can you help me ?")
+                .build();
+    }
+}
