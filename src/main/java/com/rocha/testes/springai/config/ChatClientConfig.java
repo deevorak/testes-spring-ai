b34a0a7ab5e22a918c03f6410a1dba273611cc56
Carlos Rocha <carlosesr.inf@gmail.com>
2026-05-17 11:15:50 -0300

    upgrade java 25 and setting gemini ai

--- src/main/java/com/rocha/testes/springai/config/ChatClientConfig.java
+++ src/main/java/com/rocha/testes/springai/config/ChatClientConfig.java
@@ -1,6 +1,7 @@
 package com.rocha.testes.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +24,7 @@ public class ChatClientConfig {
                         kindly inform them that you can only assist with queries related to\s
                         HR policies.
                         """)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultUser("How can you help me ?")
                 .build();
     }
